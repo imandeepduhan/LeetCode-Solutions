e2eae906c9e2a4b776831f4ccff5047aa53b3fa9
𝗠𝗮𝗻𝗱𝗲𝗲𝗽 𝗗𝘂𝗵𝗮𝗻 <imandeepduhan@gmail.com>
2026-09-03 16:24:35 +0530

    Create 0217_ContainsDuplicate.java

---
+++ 0217_ContainsDuplicate.java
@@ -0,0 +1,22 @@
+/*
+Problem Name : 217 Contains Duplicate
+Problem Link : https://leetcode.com/problems/contains-duplicate/description/
+
+My Approach : I use a HashSet to remove duplicate elements. Finally, I check if nums.length == set.size(). If they are equal, there are no duplicates, so I return false. Otherwise, I return true.
+
+*/
+
+import java.util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        if(set.size() == nums.length) {
+            return false;
+        } 
+
+        return true;
+    }
+}

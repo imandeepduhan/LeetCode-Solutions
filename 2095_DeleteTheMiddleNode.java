84ea2cb6177f386dcb3133f13d64c4b11d6d16f8
𝗠𝗮𝗻𝗱𝗲𝗲𝗽 𝗗𝘂𝗵𝗮𝗻 <imandeepduhan@gmail.com>
2026-09-05 05:16:38 +0530

    Add solution for deleting the middle node in a linked list
    Implement solution to delete the middle node of a linked list using slow and fast pointers.

---
+++ 2095_DeleteTheMiddleNode.java
@@ -0,0 +1,38 @@
+/*
+Problem Name : 2095. Delete the Middle Node of a Linked List
+Problem Link : https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
+
+My Approach: First, I create slow, fast, and slowBack pointers pointing to head. Then, I check if head.next == null; if it is, I return null. Otherwise, I start a while loop where slowBack points to the previous node of slow, slow moves one step, and fast moves two steps. Finally, I delete the middle node by setting slowBack.next = slow.next and return head.
+
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode slowBack = head;
+        boolean ok = true;
+
+        if(head.next == null) {
+            return null;
+        }
+
+        while(fast != null && fast.next != null) {
+            slowBack = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slowBack.next = slow.next;
+        return head;
+    }
+}

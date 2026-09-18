0efb87d8560564876234172014c94b4763a56371
𝗠𝗮𝗻𝗱𝗲𝗲𝗽 <imandeepduhan@gmail.com>
2026-09-18 21:52:02 +0530

    Update 0023_MergeKSortedLists.java

--- 0023_MergeKSortedLists.java
+++ 0023_MergeKSortedLists.java
@@ -2,7 +2,11 @@
 Problem Name : 23. Merge k Sorted Lists
 Problem Link : https://leetcode.com/problems/merge-k-sorted-lists/description/
 
-My Approach: First, I create two arrays, negative and positive. Then, I start a for loop and store the negative elements in the negative array and the positive elements in the positive array. Then, I create a new linked list and traverse the negative array backward and add the elements to the linked list. Then, I traverse the positive array and add the elements to the linked list. Finally, I return the linked list.
+My Approach: First, I create two arrays, negative and positive.
+Then, I start a for loop and store the negative elements in the negative array.
+The positive elements in the positive array. 
+Then, I create a new linked list and traverse the negative array backward and add the elements to the linked list. 
+Then, I traverse the positive array and add the elements to the linked list. Finally, I return the linked list.
 
 */
 

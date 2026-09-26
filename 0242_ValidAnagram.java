6099096a38ab3000021e0e362ed7af27ebc8015d
𝗠𝗮𝗻𝗱𝗲𝗲𝗽 <imandeepduhan@gmail.com>
2026-09-26 08:06:57 +0530

    Refactor comments in 0242_ValidAnagram.java
    Updated comments to improve readability.

--- 0242_ValidAnagram.java
+++ 0242_ValidAnagram.java
@@ -2,7 +2,9 @@
 Problem Name : 242. Valid Anagram
 Problem Link : https://leetcode.com/problems/valid-anagram/description/
 
-My Approach : First, I create two arrays of size 26. Then, I iterate through both strings and increment the count of each character using its position (char - 'a'). Finally, I compare both arrays. If all counts are equal, I return true; otherwise, I return false.
+My Approach : First, I create two arrays of size 26. Then, I iterate through both strings 
+and increment the count of each character using its position (char - 'a'). 
+Finally, I compare both arrays. If all counts are equal, I return true; otherwise, I return false.
 */
 
 class Solution {
